08b1aa6dadc0db1447e0a952ebfc7afb3dba4e1d
ishasinghal23 <ishasinghal2002@gmail.com>
2024-03-24 16:13:50 +0530

    Fixing small errors

--- main.py
+++ main.py
@@ -7,11 +7,12 @@ from prophet.plot import plot_plotly
 from plotly import graph_objs as go
 
 START = "2015-01-01"
+
 TODAY = date.today().strftime("%Y-%m-%d")
 
 st.title("Stock Prediction App")
 
-stocks = ("AAPL", "GOOG", "MSFT", "GME")
+stocks = ("AAPL", "GOOG", "MSFT", "GME", "TATAMOTORS.NS", 'SBI')
 selected_stocks = st.selectbox("Select dataset for prediction", stocks)
 
 n_years = st.slider("Years of Predcition: ", 1, 4)
